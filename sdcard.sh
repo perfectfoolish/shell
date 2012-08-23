0cff6048171f9a0f29e8fd02566fcb363115dcc3
lifulei <az00000000@126.com>
2012-08-23 19:22:07 +0800

    Update sdcard.sh

--- sdcard.sh
+++ sdcard.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-#Copyright (C) 2012 Ceict Limited. All rights reserved.
-#
-#This file is property of Ceict with all rights reserved under the copyright laws.
-
 DIR=/sdcard/logs
 max_dirno=0
 
