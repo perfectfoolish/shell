d4462e93d19ad4536f85464dfb13818505d19166
lifulei <az00000000@126.com>
2012-08-30 13:50:48 +0800

    scripts for jenkins, but not strong ! record build release by file release, next edition record build directly by CMT.spec, don't add extra file

---
+++ jenkins01.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+rm -rf MCSS_Server CMT*
+git clone git://192.168.0.23/MCSS_Server
+
+VER=`egrep '[0-9]{1}\.[0-9]{1}\.[0-9]{1}\.[0-9]{1}' MCSS_Server/ceictserver/.version | cut -c 1-6`
+TODAY=`date +%Y%m%d`
+VERSION=$VER$TODAY
+RELEASE=1
+
+chmod -R +x MCSS_Server
+
+if [ ! -f release ]; then
+    echo "DATE=$TODAY" > release
+
+    echo "don't have release! touch release today first build 1"
+    mv ./MCSS_Server/ceictserver CMT-$VERSION-$RELEASE
+    tar zcf CMT-$VERSION-$RELEASE.tar.gz CMT-$VERSION-$RELEASE
+    mv CMT-$VERSION-$RELEASE.tar.gz SOURCES/
+    sed -i "s/^Version:.*/Version: $VERSION/" SPECS/CMT.spec
+    sed -i "s/^Release:.*/Release: $RELEASE/" SPECS/CMT.spec
+    rpmbuild --define '_topdir '`pwd` -bb SPECS/CMT.spec
+    RELEASE=$(($RELEASE+1))
+    echo "Release=$RELEASE" >> release
+
+else
+
+    TIME=`sed -n "/DATE=/s/DATE=//p" release`
+    NO=`sed -n "/Release=/s/Release=//p" release`
+    if [ "$TIME" -eq "$TODAY" ]; then
+
+        echo "today next build time $TIME $NO"
+        mv ./MCSS_Server/ceictserver CMT-$VERSION-$NO
+        tar zcf CMT-$VERSION-$NO.tar.gz CMT-$VERSION-$NO
+        mv CMT-$VERSION-$NO.tar.gz SOURCES/
+        sed -i "s/^Version:.*/Version: $VERSION/" SPECS/CMT.spec
+        sed -i "s/^Release:.*/Release: $NO/" SPECS/CMT.spec
+        rpmbuild --define '_topdir '`pwd` -bb SPECS/CMT.spec
+        RELEASE=$(($NO+1))
+        sed -i "s/^Release=.*/Release=$RELEASE/" release
+    else
+
+        echo "already have release! today first 1 time $TIME $NO"
+        mv ./MCSS_Server/ceictserver CMT-$VERSION-$RELEASE
+        tar zcf CMT-$VERSION-$RELEASE.tar.gz CMT-$VERSION-$RELEASE
+        mv CMT-$VERSION-$RELEASE.tar.gz SOURCES/
+        sed -i "s/^Version:.*/Version: $VERSION/" SPECS/CMT.spec
+        sed -i "s/^Release:.*/Release: $RELEASE/" SPECS/CMT.spec
+        rpmbuild --define '_topdir '`pwd` -bb SPECS/CMT.spec
+        sed -i "s/^DATE=.*/DATE=$TODAY/" release
+        RELEASE=$(($RELEASE+1))
+        sed -i "s/^Release=.*/Release=$RELEASE/" release
+    fi
+fi
